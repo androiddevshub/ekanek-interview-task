605689e343a3a983989fe87380e9a23484a93a25
Shubham jain <beardo@192.168.1.6>
2022-03-21 14:30:29 +0530

    Configure react with rails

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   mount Api => "/"
+  devise_for :users
+  root to: 'welcome#index'
+  get "/*path", to: "welcome#index", constraints: ->(request) do
+                  !request.xhr?
+                end
 end
