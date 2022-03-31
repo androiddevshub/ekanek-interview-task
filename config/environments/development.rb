b14ec20b35a231cac0f0d57babaf0c01492c76c7
Shubham jain <beardo@Shubhams-MacBook-Pro-2.local>
2022-04-01 00:17:56 +0530

    Move app host to env

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -30,7 +30,7 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
-  Rails.application.routes.default_url_options[:host] = 'localhost:3000'
+  Rails.application.routes.default_url_options[:host] = ENV['APP_HOST']
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
